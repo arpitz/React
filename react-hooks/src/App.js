12337121ab3cc7392e1589fae8ab7b607f660871
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-04-15 17:04:25 +0530

    Custom Hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Form from './components/customHooks/Form';
+import DocTitleOne from './components/customHooks/DocTitleOne';
+import DocTitleTwo from './components/customHooks/DocTitleTwo';
 
 function App() {
   return (
     <div className="App">
-    <Form/>
+    <DocTitleOne />
+    <DocTitleTwo/>
     </div>
   );
 }
