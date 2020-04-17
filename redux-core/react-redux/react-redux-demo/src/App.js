6f938b5fc55724e44000cc495bece1230f9082b5
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-04-17 19:30:33 +0530

    map state to props

--- redux-core/react-redux/react-redux-demo/src/App.js
+++ redux-core/react-redux/react-redux-demo/src/App.js
@@ -5,14 +5,19 @@ import CakeContainer from './components/CakeContainer';
 import store from './redux/store';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer/>
+        <NewCakeContainer/>
       </div>
     </Provider>
   );
