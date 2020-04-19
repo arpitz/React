925d335a9167aac672f6cceab574cbb5dddd3bba
Arpit Trivedi <arpit1trivedi@gmail.com>
2020-04-19 14:38:26 +0530

    Added Countup in Infected card

--- covid_19_tracker/src/components/Cards/Cards.jsx
+++ covid_19_tracker/src/components/Cards/Cards.jsx
@@ -1,17 +1,48 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 import styles from './Cards.module.css';
 
-const Cards = () => {
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate }}) => {
+    if(!confirmed){
+        return(
+            <div>Loading ...</div>
+        )
+    }
     return(
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
                 <Grid item component={ Card }>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5"> Data</Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
                         <Typography color="textSecondary">Real Date</Typography>
                         <Typography variant = "body2">Number of active cases of Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={ Card }>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5"> Data</Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant = "body2">Number of recovered cases of Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={ Card }>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5"> Data</Typography>
+                        <Typography color="textSecondary">Real Date</Typography>
+                        <Typography variant = "body2">Number of deaths of Covid-19</Typography>
                     </CardContent>
                 </Grid>
             </Grid>
